43939a4220a923d101cd11b3cf4f5af899e0c026
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-02 21:02:39 +0800

    Update products.blade.php

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -2,4 +2,35 @@
 
 @section('sidenav')
 
+<main>
+    <div class="card-deck">
+        <div class="card">
+          <div class="card-body text-center">
+            <h4 class="card-title">Product Name</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptates deleniti distinctio molestias alias, nobis deserunt aperiam laborum accusantium laudantium ducimus, adipisci odit ad illum porro debitis nemo voluptatibus doloremque!</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body text-center">
+            <h4 class="card-title">Product Name</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ex commodi dolor, quaerat non incidunt eius necessitatibus voluptates neque, cupiditate soluta ipsam. Eum, ab voluptates! Dolorum ut provident odio non!</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body text-center">
+            <h4 class="card-title">Product Name</h4>
+            <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur ea ab facere exercitationem soluta ipsum voluptatum earum at distinctio. In maiores vitae alias tenetur a placeat quo eum praesentium ad!</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body text-center">
+            <h4 class="card-title">Product Name</h4>
+            <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum laudantium facilis reprehenderit eveniet eius distinctio ipsam doloribus rem, blanditiis vel rerum dolor tenetur sapiente aperiam consectetur temporibus adipisci magnam tempore?</p>
+          </div>
+        </div>
+      </div>
+</main>
+
+    
+
 @endsection
