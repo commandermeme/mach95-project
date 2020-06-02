7e078726ab89a46ceed0208b38a2edccaae53aa2
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-02 20:25:02 +0800

    Create home.blade.php

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.topnav')
+
+@section('content')
+<h1>Shop Now!</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nulla mollitia ab voluptatem officiis incidunt culpa quaerat temporibus enim laboriosam quasi sapiente, consectetur necessitatibus, reiciendis rerum eum? Est, error eveniet.</p>
+@endsection
