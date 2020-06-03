6370e8739e3c296e9fdb8c2f498f311677bc3fea
commandermeme <carlobahog1@gmail.com>
2020-06-03 13:53:34 +0800

    Initial commit

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,38 @@
+
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-home"></i> Dashboard
+                </a>
+            </li>
+
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-people"></i> Clients
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-car"></i> Vehicles
+                </a>
+            </li>
+
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-warning"></i> Violations
+                </a>
+            </li>
+
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-map-marker"></i> Regulations
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin', function () {
+    return redirect('login');
+});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
