2f022ba8ad21cd58c65795c0085ca1282fa45f03
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-03 16:05:43 +0800

    Update products.blade.php

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -9,18 +9,24 @@
                 <div class="card-body text-center">
                     <h5>Product 1</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore corporis earum molestias rerum mollitia explicabo quam eos reiciendis velit aspernatur id. Dignissimos, expedita optio! Eligendi ipsa odit iste voluptatum.</p>
+                    <button class="btn btn-primary btn-sm float-left" style="color:white">Buy </button>                     
+                    <button class="btn btn-success btn-sm float-right" style="color:white">Buy </button>                     
                 </div>
             </div>
             <div class="card">
                 <div class="card-body text-center">
                     <h5>Product 2</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore corporis earum molestias rerum mollitia explicabo quam eos reiciendis velit aspernatur id. Dignissimos, expedita optio! Eligendi ipsa odit iste voluptatum.</p>
+                    <button class="btn btn-primary btn-sm float-left" style="color:white">Buy </button>                     
+                    <button class="btn btn-success btn-sm float-right" style="color:white">Buy </button>
                 </div>
             </div>
             <div class="card">
                 <div class="card-body text-center">
                     <h5>Product 3</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore corporis earum molestias rerum mollitia explicabo quam eos reiciendis velit aspernatur id. Dignissimos, expedita optio! Eligendi ipsa odit iste voluptatum.</p>
+                    <button class="btn btn-primary btn-sm float-left" style="color:white">Buy </button>                     
+                    <button class="btn btn-success btn-sm float-right" style="color:white">Buy </button>
                 </div>
             </div>
         </div>
