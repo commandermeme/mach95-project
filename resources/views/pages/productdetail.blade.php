0035e651597d25840f3f5d9cf94ce7cdac77cf48
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-03 15:56:17 +0800

    Update productdetail.blade.php

--- resources/views/pages/productdetail.blade.php
+++ resources/views/pages/productdetail.blade.php
@@ -2,23 +2,20 @@
 
 @section('topnav')
 
-<div class="container pt-5">
+<div class="container">
     <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card">
+        <div class="col-md-4 pt-5">
+            <div class="card" id="item">
                 <img class="card-img-top" src="img/glasses1.jpg" alt="Card image">
-                <div class="card-body">
-                  <h4 class="card-title">John Doe</h4>
-                  <p class="card-text">Some example text.</p>
-                  <a href="#" class="btn btn-primary">See Profile</a>
-                </div>
-              </div>
+                <div class="card-footer text-center mt-auto" style="border-top:0">     
+                    <button class="btn btn-primary btn-sm float-left" id="left" style="color:white">Button1 </button>
+                    <button class="btn btn-success btn-sm float-right" id="left" style="color:white">Button2 </button>                                          
+                </div> 
+            </div>
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col">
-            <h1>Product name</h1>
+        <div class="col-m-8 mx-auto pt-5">
+            <h1>Product 1</h1>
+            <p class="lead">This is Product 1.<br>Price : 1,000 </p>
         </div>
     </div>
 </div>
