7090e80781ba837cf28574a484ae40fca44419ae
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-03 14:32:43 +0800

    Update topnav.blade.php

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -8,6 +8,7 @@
     <link type="text/css" rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     
     </head>
 
@@ -30,10 +31,12 @@
                     <a class="nav-link" href="#">Contact</a>
                 </li>
               </ul>
+
               <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" label="Search">
                 <button class="btn my-2 my-sm-0" type="submit">Search</button>
               </form>
+
             </div>
           </nav>
           @yield('topnav')
