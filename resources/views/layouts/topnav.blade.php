46d0a13979e85e09e4868504234bed3b0a04b300
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-03 22:37:40 +0800

    Update topnav.blade.php

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -13,8 +13,8 @@
     </head>
 
     <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#"> We Shop</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand ml-5 pl-5" href="#">We<span style="color: #ff003c">    Shop</span></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
