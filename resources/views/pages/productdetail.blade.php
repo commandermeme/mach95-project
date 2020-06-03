47fa3c06623cfef6081be4f1403f50cbc0e1e1ec
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-03 14:32:48 +0800

    Create productdetail.blade.php

---
+++ resources/views/pages/productdetail.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.topnav')
+
+@section('topnav')
+
+<div class="container pt-5">
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="card">
+                <img class="card-img-top" src="img/glasses1.jpg" alt="Card image">
+                <div class="card-body">
+                  <h4 class="card-title">John Doe</h4>
+                  <p class="card-text">Some example text.</p>
+                  <a href="#" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <h1>Product name</h1>
+        </div>
+    </div>
+</div>
+    
+@endsection
