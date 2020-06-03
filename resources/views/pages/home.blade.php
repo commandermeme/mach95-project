20926f83fc6ad6d0fe17a041743587bbbb4b6e88
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-03 20:08:44 +0800

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('topnav')
 
-<header class="jumbotron jumbotron-fluid">
+<header class="jumbotron">
     <div class="container-fluid text-center"> 
-       <h1 class="display-3">Marlyn Cuico</h1>
+       <h1 class="display-3">Marlyn's Shop</h1>
        <p class="lead pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe dicta sapiente harum odio repellendus facilis qui et illum sit delectus iusto in expedita consequuntur accusamus officia ad, maxime ut suscipit?</p>
        <p><a href="#" class="btn btn-primary btn-lg" role="button">Shop now!</a></p>
     </div>
