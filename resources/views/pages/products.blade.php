3dd1626a5d57fe361abb150c04ddecdf77e2c3bc
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-03 10:05:15 +0800

    Update products.blade.php

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -13,13 +13,13 @@
             </div>
             <div class="card">
                 <div class="card-body text-center">
-                    <h5>Product 1</h5>
+                    <h5>Product 2</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore corporis earum molestias rerum mollitia explicabo quam eos reiciendis velit aspernatur id. Dignissimos, expedita optio! Eligendi ipsa odit iste voluptatum.</p>
                 </div>
             </div>
             <div class="card">
                 <div class="card-body text-center">
-                    <h5>Product 1</h5>
+                    <h5>Product 3</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, tempore corporis earum molestias rerum mollitia explicabo quam eos reiciendis velit aspernatur id. Dignissimos, expedita optio! Eligendi ipsa odit iste voluptatum.</p>
                 </div>
             </div>
