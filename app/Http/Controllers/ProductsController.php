0108c371fa174e36cef92196eb564d3da389db6b
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-04 14:01:24 +0800

    Update ProductsController.php

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -1,10 +1,46 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Product;
 
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+    public function index($category)
+    {
+        if($category == 'all')
+        {
+            $products =  Product::all();
+            return view('pages.products')->with('products', $products);
+        }
+        else if($category == 'eyeglasses')
+        {
+            
+        return 'eyeglasses';
+        }
+        else if($category == 'necklace')
+        {
+            return 'necklace';
+        }
+        else if($category == 'earrings')
+        {
+            return 'earrings';
+        }
+        else if($category == 'hat')
+        {
+            return 'hat';
+        }
+    }
+
+    public function show($id) 
+    {
+        $product = Product::find($id);
+        return view('pages.productdetail')->with('product', $product);
+    }
+
+    public function eyeglasses()
+    {
+        return 'eyeglasses';
+    }
 }
