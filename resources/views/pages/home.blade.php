8fe3261e3953f8e3ce25f6f1995aae7d31adb375
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-04 14:02:11 +0800

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid text-center"> 
        <h1 class="display-3">Marlyn's Shop</h1>
        <p class="strong pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe dicta sapiente harum odio repellendus facilis qui et illum sit delectus iusto in expedita consequuntur accusamus officia ad, maxime ut suscipit?</p>
-       <p><a href="#" class="btn btn-primary btn-lg" role="button">Shop now!</a></p>
+        <p><a href="/products/{{'all'}}" class="btn btn-primary btn-lg" role="button">Shop now!</a></p>
     </div>
 </header>
 
