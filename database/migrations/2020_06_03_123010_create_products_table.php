946aebf0a3dec1df4040383fa96c282db718951d
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-04 20:45:55 +0800

    Update 2020_06_03_123010_create_products_table.php

--- database/migrations/2020_06_03_123010_create_products_table.php
+++ database/migrations/2020_06_03_123010_create_products_table.php
@@ -20,6 +20,7 @@ class CreateProductsTable extends Migration
             $table->string('type');
             $table->string('description');
             $table->float('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
