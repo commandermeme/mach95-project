65b613bdaa8061ae099031675dc3a5286ae35638
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-04 19:55:29 +0800

    Update products.blade.php

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -9,14 +9,16 @@
                 <div class="col-md-4">
                     <div class="card-deck pt-4" id="product">
                         <div class="card">
-                            <div class="card-body text-center">
+                            <div class="card-body text-center" style="height: 250px">
                             <h5><a href="/productdetail/{{$product->id}}" > {{$product->product_name}} </a></h5>
                             </div>
                             <img class="card-img" src="img/glasses1.jpg" alt="Card image">
                             <p class="card-text text-center"> {{$product->description}} </p>
-                            {{-- <p> {{$product->price}}</p> --}}
                             <div class="card-footer text-center mt-auto" style="border-top:0">     
-                                <a href="/checkout"><button class="btn btn-sm" style="color:white">Buy</button></a>
+                                <a href="/checkout"><button class="btn btn-sm" 
+                                    style="color:white; background-color: #ff003c;">
+                                    Buy</button>
+                                </a>
                             </div>
                         </div>
                     </div>
