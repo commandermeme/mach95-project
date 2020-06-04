1a125f637daa1ad337dd57050f497e1bdf9948c7
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-04 20:46:04 +0800

    Update productdetail.blade.php

--- resources/views/pages/productdetail.blade.php
+++ resources/views/pages/productdetail.blade.php
@@ -4,19 +4,19 @@
 
 <div class="container">
     <div class="row">
-        {{-- <div class="col-md-4 pt-5">
-            <div class="card" id="item">
-                <img class="card-img" src="img/glasses1.jpg" alt="Card image">
-                <div class="card-footer text-center mt-auto" style="border-top:0">     
-                    <button class="btn btn-sm">Return</button>
-                    <button class="btn btn-sm">Buy</button>
-                </div> 
+        <div class="col-md-4 pt-5">
+            <div class="card" id="item" style="height: 550px">
+                <img class="card-img my-auto" src="/img/{{$product->image}}" alt="Card image">
+                {{-- <div class="card-footer text-center mt-auto" style="border-top:0">     
+                    <button class="btn btn-md">Return</button>
+                    <button class="btn btn-md">Buy</button>
+                </div>  --}}
             </div>
-        </div> --}}
+        </div>
         <div class="col-m-8 mx-auto pt-5">
             <h1> {{$product->product_name}}</h1>
-            <p class="lead"> {{$product->description}} <br> {{$product->price}} </p>
-            <a href="/products"><button class="btn btn-md btn-primary" style="width: 70px">Return</button></a>
+            <p class="lead"> {{$product->description}} <br> Price : {{$product->price}} </p>
+            <a href="/products/{{'all'}}"><button class="btn btn-md btn-primary" style="width: 70px">Return</button></a>
             <a href="/checkout"><button class="btn btn-md" id="detailbtn" style="background-color: #ff003c; color:white; width: 70px">Buy</button></a>
         </div>
     </div>
