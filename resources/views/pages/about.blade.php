1697fdee79f7564a61efa483fc70c11fd7f29e79
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-06-04 14:01:59 +0800

    Update about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.topnav')
+
+@section('topnav')
+
+<header class="about">
+    <div class="container-fluid text-center"> 
+       <h1 class="display-3">Marlyn's Shop</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor corporis voluptatum reprehenderit hic enim dolores. Corrupti ab, aut, minima est enim officiis culpa nisi provident, hic in rem nobis? Assumenda.</p>
+    </div>
+</header>
+
+@endsection 
